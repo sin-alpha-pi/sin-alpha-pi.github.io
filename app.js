eef158359491a8ec5a533d77c6f322c5e2f31bf9
sin-alpha-pi <53899103+sin-alpha-pi@users.noreply.github.com>
2020-07-18 00:24:14 +0200

    Made it nicer

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-const fetch = require("node-fetch");
-const clientId = "fbb75d3e11e44656917e9c6a9a9a301d"
-const clientSecret = "9a94271741d547d9a365bf6327363156"
-const encodedAuthString = new Buffer(clientId + ":" + clientSecret).toString("base64");
-songId = "7ytR5pFWmSjzHJIeQkgog4"
+console.log("hey");
 
-fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        body: 'grant_type=client_credentials',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': `Basic ${encodedAuthString}`
-        }
-    })
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-        console.log(data.access_token)
+//const fetch = require("node-fetch");
+const clientId = "fbb75d3e11e44656917e9c6a9a9a301d";
+const clientSecret = "9a94271741d547d9a365bf6327363156";
+const encodedAuthString = btoa(clientId + ":" + clientSecret);
+const songId = "7ytR5pFWmSjzHJIeQkgog4";
 
-        fetch(`https://api.spotify.com/v1/tracks/${songId}`, {
-                headers: {
-                    'Authorization': "Bearer " + data.access_token
-                }
-            })
-            .then(function(resp) {
-                return resp.json()
-            })
-            .then(function(data) {
-                console.log(data.popularity)
-            })
-    })
+function main() {
+    fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            body: 'grant_type=client_credentials',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': `Basic ${encodedAuthString}`
+            }
+        })
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(function(data) {
+            fetch(`https://api.spotify.com/v1/tracks/${songId}`, {
+                    headers: {
+                        'Authorization': "Bearer " + data.access_token
+                    }
+                })
+                .then(function(resp) {
+                    return resp.json()
+                })
+                .then(function(data) {
+                    console.log(data)
+                })
+        })
+}
+main()
